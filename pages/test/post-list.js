f5d401e376762c76b2f5a64675a995af06096898
searchmoon <moonding901212@gmail.com>
2022-10-18 01:27:17 +0900

    서버연동 post

--- pages/test/post-list.js
+++ pages/test/post-list.js
@@ -1,59 +1,66 @@
-import {useRequestQuery} from "../hooks/NetworkCustomHooks";
-import React, {useCallback, useState, useRef} from "react";
-import {Formik, Field} from 'formik';
+import { useRequestQuery } from "../hooks/NetworkCustomHooks";
+import React, { useCallback, useState, useRef } from "react";
+import { Formik, Field } from "formik";
 import * as Yup from "yup";
-
 
 //useRef 변수를 포믹 폼에다가 연결해라
 function PostList() {
-    const createRequest = useRequestQuery();
-    const formikRef = useRef(null);
-    const handleSubmitForm = useCallback(async (values) => {
-        console.log('values', values);
+  const createRequest = useRequestQuery();
+  const formikRef = useRef(null);
+  const handleSubmitForm = useCallback(
+    async (values) => {
+      console.log(values);
 
-        await createRequest().post("http://localhost:8080/sample")
-            .setFormikRef(formikRef.current)
-            .setData(values).setSuccessFunc((resp) => {
-                console.log('server result ', resp);
-            }).run();
-    }, [formikRef.current])
+      await createRequest()
+        .post("http://localhost:8080/sample")
+        .setFormikRef(formikRef.current)
+        .setData(values)
+        .setSuccessFunc((resp) => {
+          console.log("server result ", resp);
+        })
+        .run();
+    },
+    [formikRef.current]
+  );
 
-
-    return (
-        <>
-            <Formik
-                initialValues={{
-                    name: "",
-                    desc: "",
-                    level: "",
-                }}
-                innerRef={formikRef}
-                // validationSchema={validationSchema}
-                onSubmit={handleSubmitForm}
+  return (
+    <>
+      <Formik
+        initialValues={{
+          name: "",
+          desc: "",
+          level: "",
+        }}
+        innerRef={formikRef}
+        // validationSchema={validationSchema}
+        onSubmit={handleSubmitForm}
+      >
+        {({ handleSubmit, errors, isSubmitting }) => (
+          <>
+            <div>
+              name: <Field name="name" />
+              {errors.name && <div>{errors.name}</div>}
+            </div>
+            <div>
+              desc: <Field name="desc" />
+              {errors.desc && <div>{errors.desc}</div>}
+            </div>
+            <div>
+              level: <Field name="level" />
+              {errors.level && <div>{errors.level}</div>}
+            </div>
+            <button
+              type={"button"}
+              isSubmitting={isSubmitting}
+              onClick={handleSubmit}
             >
-                {({handleSubmit, errors, isSubmitting}) => (
-                    <>
-                        <div>
-                            name: <Field name="name"/>
-                            {errors.name && <div>{errors.name}</div>}
-                        </div>
-                        <div>
-                            desc: <Field name="desc"/>
-                            {errors.desc && <div>{errors.desc}</div>}
-                        </div>
-                        <div>
-                            level: <Field name="level"/>
-                            {errors.level && <div>{errors.level}</div>}
-                        </div>
-                        <button type={"button"} isSubmitting={isSubmitting} onClick={handleSubmit}>
-                            {isSubmitting ? '빙글뱅글' : '추가'}
-                        </button>
-                    </>
-                )}
-            </Formik>
-        </>
-    )
-
+              {isSubmitting ? "빙글뱅글" : "추가"}
+            </button>
+          </>
+        )}
+      </Formik>
+    </>
+  );
 }
 
 export default PostList;
--- pages/test/post-list2.js
+++ pages/test/post-list2.js
@@ -1,59 +1,72 @@
-import {useRequestQuery} from "../hooks/NetworkCustomHooks";
-import React, {useCallback, useState, useRef} from "react";
-import {Formik, Field} from 'formik';
+import { useRequestQuery } from "../hooks/NetworkCustomHooks";
+import React, { useCallback, useState, useRef } from "react";
+import { Formik, Field } from "formik";
 import * as Yup from "yup";
-
 
 //useRef 변수를 포믹 폼에다가 연결해라
 function PostList() {
-    const createRequest = useRequestQuery();
-    const formikRef = useRef(null);
-    const handleSubmitForm = useCallback(async (values) => {
-        console.log('values', values);
+  const createRequest = useRequestQuery();
+  const formikRef = useRef(null);
+  const handleSubmitForm = useCallback(
+    async (values) => {
+      console.log("values", values);
 
-        await createRequest().post("http://localhost:8080/sample")
-            .setFormikRef(formikRef.current)
-            .setData(values).setSuccessFunc((resp) => {
-                console.log('server result ', resp);
-            }).run();
-    }, [formikRef.current])
+      await createRequest()
+        .post("http://localhost:8080/create")
+        .setFormikRef(formikRef.current)
+        .setData(values)
+        .setSuccessFunc((resp) => {
+          console.log("server result ", resp);
+        })
+        .run();
+    },
+    [formikRef.current]
+  );
 
-
-    return (
-        <>
-            <Formik
-                initialValues={{
-                    name: "",
-                    desc: "",
-                    level: "",
-                }}
-                innerRef={formikRef}
-                // validationSchema={validationSchema}
-                onSubmit={handleSubmitForm}
+  return (
+    <>
+      <Formik
+        initialValues={{
+          username: "",
+          name: "",
+          password: "",
+          age: "",
+        }}
+        innerRef={formikRef}
+        // validationSchema={validationSchema}
+        onSubmit={handleSubmitForm}
+      >
+        {({ handleSubmit, errors, isSubmitting }) => (
+          <>
+            <div>
+              username: <Field name="username" />
+              {errors.username && <div>{errors.username}</div>}
+            </div>
+            <div>
+              name: <Field name="name" />
+              {errors.name && <div>{errors.name}</div>}
+            </div>
+            <div>
+              password: <Field name="password" />
+              {errors.password && <div>{errors.password}</div>}
+            </div>
+            <div>
+              age: <Field name="age" />
+              {errors.age && <div>{errors.age}</div>}
+            </div>
+            <button
+              type={"button"}
+              isSubmitting={isSubmitting}
+              onClick={handleSubmit}
             >
-                {({handleSubmit, errors, isSubmitting}) => (
-                    <>
-                        <div>
-                            name: <Field name="name"/>
-                            {errors.name && <div>{errors.name}</div>}
-                        </div>
-                        <div>
-                            desc: <Field name="desc"/>
-                            {errors.desc && <div>{errors.desc}</div>}
-                        </div>
-                        <div>
-                            level: <Field name="level"/>
-                            {errors.level && <div>{errors.level}</div>}
-                        </div>
-                        <button type={"button"} isSubmitting={isSubmitting} onClick={handleSubmit}>
-                            {isSubmitting ? '빙글뱅글' : '추가'}
-                        </button>
-                    </>
-                )}
-            </Formik>
-        </>
-    )
-
+              추가
+              {/* {isSubmitting ? "빙글뱅글" : "추가"} */}
+            </button>
+          </>
+        )}
+      </Formik>
+    </>
+  );
 }
 
 export default PostList;
